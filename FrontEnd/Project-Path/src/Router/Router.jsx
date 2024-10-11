ea01a31ce3f7c2569d7be63a17e361ea28c57181
Yousef M <yousefmalsaadan@gmail.com>
2024-10-11 20:20:37 +0300

    creat yousef branch

--- FrontEnd/Project-Path/src/Router/Router.jsx
+++ FrontEnd/Project-Path/src/Router/Router.jsx
@@ -1,12 +1,20 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
- 
+import Login from "../Pages/Login";
+import Signup from "../Pages/Signup";
+
 const Router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
   },
- 
- 
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/signup",
+    element: <Signup />,
+  },
 ]);
 export default Router;
