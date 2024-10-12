d7be27babf934795455ac7eb196ff0080c0e04ff
unknown <madaawadh1@gmail.com>
2024-10-12 22:33:50 +0300

    cards

--- FrontEnd/Project-Path/src/Pages/Dashboard.jsx
+++ FrontEnd/Project-Path/src/Pages/Dashboard.jsx
@@ -1,20 +1,24 @@
-
+import { Link } from "react-router-dom";
 import Nav from '../Component/Nav';
 function Dashboard() {
   return <>
   <section className='w-full flex '>
   <Nav/>
-  <section className="p-8 w-4/5">
-  <h2 className='font-bold text-2xl'>UserName</h2>
-  <div className='flex justify-around '>
-    <div className="w-[350px] rounded-md border-solid border-2 border-[#26dec6]">
-    <img src="https://i.ibb.co/R4G2WJ2/student.jpg" alt="" srcset="" className='w-'/>
-    <h2 className='font-bold text-2xl'>Projects</h2>
+  <section className="p-8 md:w-4/5 xs:w-full">
+  <h2 className='font-bold text-2xl p-5'>UserName</h2>
+  <div className='flex justify-around flex-wrap gap-2 '>
+    <Link to="">
+    <div className="w-[330px] h-[180px] rounded-md border-solid border-2 border-[#26dec6] pr-5 flex items-center justify-center">
+    <img src="https://i.ibb.co/rtzrftR/students.jpg" alt="" srcset="" className='w-1/5 pb-12'/>
+    <h2 className='font-bold text-4xl'>Projects</h2>
     </div>
-    <div className="w-[350px] rounded-md ">
-    <img src="https://i.ibb.co/R4G2WJ2/student.jpg" alt="" srcset="" />
-    <h2 className='font-bold text-2xl'>Students</h2>
+    </Link>
+    <Link to="">
+    <div className="w-[330px] h-[180px] rounded-md border-solid border-2 border-[#26dec6] pr-5 flex items-center justify-center">
+    <img src="https://i.ibb.co/R4G2WJ2/student.jpg" alt="" srcset="" className='w-1/5 pb-12'/>
+    <h2 className=' font-bold text-4xl'>Students</h2>
     </div>
+    </Link>
   </div>
   </section>
   </section>
