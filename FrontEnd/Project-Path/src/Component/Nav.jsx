66290b2c1b1531f1985157139786cce394a74723
unknown <madaawadh1@gmail.com>
2024-10-12 23:30:49 +0300

    nav -cards

--- FrontEnd/Project-Path/src/Component/Nav.jsx
+++ FrontEnd/Project-Path/src/Component/Nav.jsx
@@ -8,12 +8,12 @@ function Nav() {
     document.querySelector("nav").style.right="0%"
    }
   return <>
-   <img className="md:invisible xs:visible absolute left-4 top-4 " src="https://i.ibb.co/172TmgX/menu.png" onClick={show} />
-  <nav id="nav" className="bg-[white] md:w-1/5 md:shadow-2xl xs:shadow-0 xs:w-5/5 xs:absolute z-1 flex flex-col justify-between" style={{height:"100vh",maxHeight:"100%"}} >
-  <img className="md:invisible xs:visible absolute left-[85%] top-4 z-1" src="https://i.ibb.co/d0dgRNS/close.png" onClick={hide}/>
+   <img className="sm:invisible xs:visible absolute left-4 top-4 " src="https://i.ibb.co/172TmgX/menu.png" onClick={show} />
+  <nav id="nav" className="bg-[white] sm:w-1/5 md:shadow-2xl xs:shadow-0 xs:w-5/5 xs:absolute z-1 flex flex-col justify-between " style={{height:"100vh",maxHeight:"100%"}} >
+  <img className="msm:invisible xs:visible absolute left-[85%] top-4 z-1" src="https://i.ibb.co/d0dgRNS/close.png" onClick={hide}/>
  
 <ul className="text-[#6d6d6d] " >
-<li  className="text-2xl font-bold flex items-center justify-center text-[black]" >project<span className="text-[#26dec6]">path </span><img className="w-1/5 max-w-12 pl-1" src="https://i.ibb.co/4Z3LnpB/logo-g.png" alt="" srcset="" /></li>
+<li  className="text-xl font-bold flex items-center justify-center text-[black]" >project<span className="text-[#26dec6]">path </span><img className="w-1/5 max-w-12 px-2 " src="https://i.ibb.co/4Z3LnpB/logo-g.png" alt="" srcset="" /></li>
 <Link to="/dashboard"> <li className=" p-3 flex items-center"> <img src="https://i.ibb.co/W073rqm/four-squares-button-of-view-options.png" alt="" srcset="" className="w-3 mr-3"/> Dashboard</li></Link>
 <Link to="">  <li className="p-3 flex items-center"><img src="https://i.ibb.co/W073rqm/four-squares-button-of-view-options.png" alt="" srcset="" className="w-3 mr-3"/> Students</li></Link>
 <Link to=""><li className="p-3 flex items-center"><img src="https://i.ibb.co/W073rqm/four-squares-button-of-view-options.png" alt="" srcset="" className="w-3 mr-3"/> Projects</li></Link>
