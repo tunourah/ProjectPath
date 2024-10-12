e052a6511ff6d4017fb19036218a36f9561a3679
unknown <madaawadh1@gmail.com>
2024-10-12 21:41:21 +0300

    merge

--- FrontEnd/Project-Path/src/Pages/Dashboard.jsx
+++ FrontEnd/Project-Path/src/Pages/Dashboard.jsx
@@ -2,7 +2,16 @@
 import Nav from '../Component/Nav';
 function Dashboard() {
   return <>
+  <section className='w-full flex '>
   <Nav/>
+  <section className="p-8">
+  <h2 className='font-bold text-2xl'>UserName</h2>
+  <div className='flex justify-around'>
+    <div className="w-2/6 rounded-md border-solid border-2 border-[#26dec6]"></div>
+    <div className="w-2/6 rounded-md"></div>
+  </div>
+  </section>
+  </section>
   </>;
 }
 
