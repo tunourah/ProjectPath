c19f3b76535acca8f0f0b40bc30020077fdaf2fb
unknown <madaawadh1@gmail.com>
2024-10-12 20:57:56 +0300

    nav

--- FrontEnd/Project-Path/src/Router/Router.jsx
+++ FrontEnd/Project-Path/src/Router/Router.jsx
@@ -1,10 +1,33 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
  import Dashboard from '../Pages/Dashboard'
+import Login from "../Pages/Login";
+import Signup from "../Pages/Signup";
+import ErrorPage from "../Pages/ErrorPage";
+
+import Addideas from "../Pages/Addideas"
+import Ideas from "../Pages/Ideas"
 const Router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/signup",
+    element: <Signup />,
+  },
+  {
+    path: "/Ideas",
+    element: <Ideas />,
+  },
+  {
+    path: "/AddIdeas",
+    element: <Addideas />,
   },
  
   {
