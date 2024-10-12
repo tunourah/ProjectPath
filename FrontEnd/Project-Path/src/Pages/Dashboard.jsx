21158ccba067dd3dbb3512a5cdfef6593bfa8bb7
unknown <madaawadh1@gmail.com>
2024-10-12 22:10:42 +0300

    fix

--- FrontEnd/Project-Path/src/Pages/Dashboard.jsx
+++ FrontEnd/Project-Path/src/Pages/Dashboard.jsx
@@ -4,11 +4,17 @@ function Dashboard() {
   return <>
   <section className='w-full flex '>
   <Nav/>
-  <section className="p-8">
+  <section className="p-8 w-4/5">
   <h2 className='font-bold text-2xl'>UserName</h2>
-  <div className='flex justify-around'>
-    <div className="w-2/6 rounded-md border-solid border-2 border-[#26dec6]"></div>
-    <div className="w-2/6 rounded-md"></div>
+  <div className='flex justify-around '>
+    <div className="w-[350px] rounded-md border-solid border-2 border-[#26dec6]">
+    <img src="https://i.ibb.co/R4G2WJ2/student.jpg" alt="" srcset="" className='w-'/>
+    <h2 className='font-bold text-2xl'>Projects</h2>
+    </div>
+    <div className="w-[350px] rounded-md ">
+    <img src="https://i.ibb.co/R4G2WJ2/student.jpg" alt="" srcset="" />
+    <h2 className='font-bold text-2xl'>Students</h2>
+    </div>
   </div>
   </section>
   </section>
