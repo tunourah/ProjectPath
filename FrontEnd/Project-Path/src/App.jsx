e3fc392b23139c8ad59480f13e2d701f882cedb8
noura <noura.tu19@gmail.com>
2024-10-12 09:01:42 +0300

    update style

--- FrontEnd/Project-Path/src/App.jsx
+++ FrontEnd/Project-Path/src/App.jsx
@@ -2,11 +2,8 @@ import "./App.css";
 import Table from "./Component/Table";
 
 function App() {
-  const headers = ["Student Name", "Project Name"];  
-
   return (
     <>
-      <Table headers={headers} />
     </>
   );
 }
