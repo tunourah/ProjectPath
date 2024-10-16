14a17d8420c3489a87ec3eb174cc128c6670d46e
unknown <madaawadh1@gmail.com>
2024-10-16 19:04:09 +0300

    get ideas

--- BackEnd/models/User.js
+++ BackEnd/models/User.js
@@ -6,6 +6,7 @@ const userSchema = new Schema({
   secondName: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  isAdmin:false,
   ideas:[{
 
     type: mongoose.Schema.Types.ObjectId,
