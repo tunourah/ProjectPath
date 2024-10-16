2fa5e4727ad89e7550947d163411ddb0c2551873
noura <noura.tu19@gmail.com>
2024-10-16 13:59:22 +0300

    add folders

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -63,7 +63,7 @@ app.get("/addIdea",(req,res)=>{
 
 main().catch(err => console.log(err));
 async function main() { 
-    await mongoose.connect("mongodb+srv://Mada:Mada123456@blog.mqytf.mongodb.net/");
+    await mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
   console.log("----------------////////////")
     // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
   }
